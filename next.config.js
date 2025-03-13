c1aca2e8bf1de077ee606bc3d406c1db5f0605ae
nakanohiroko <piroconbu@gmail.com>
2025-03-13 10:15:49 +0900

    [update] Next.jsのバージョンを13から15にアップデート
    ・Next.jsのバージョンを13から15にアップデート
    ・各パッケージのアップデート対応（cheerio, date-fns）
    ・CDN-Cache-Controlを指定
    ・画面プレビューキャッシュ対応
    ・canonical, openGraph.titleを各ページに設定

--- next.config.js
+++ next.config.js
@@ -8,6 +8,19 @@ const nextConfig = {
       },
     ],
   },
+  async headers() {
+    return [
+      {
+        source: '/(.*)',
+        headers: [
+          {
+            key: 'CDN-Cache-Control',
+            value: 'public, s-maxage=60, stale-while-revalidate=300',
+          },
+        ],
+      },
+    ];
+  },
 };
 
 module.exports = nextConfig;
